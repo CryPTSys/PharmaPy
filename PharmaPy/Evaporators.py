State event for steady state detection was corrected (returning float instead of int for the combined steady state criteria). Also, correct units for the moments for plots were included.

PharmaPy/Evaporators.py
@@ -540,7 +540,8 @@ class Evaporator:
 
         liquid = phase
         vapor = VaporStream(path_comp, pres=self.pres,
-                            mole_frac=np.zeros_like(liquid.mole_frac))
+                            mole_frac=liquid.mole_frac,
+                            mole_flow=eps)
 
         self._Phases = [liquid, vapor]
         classify_phases(self)

PharmaPy/Phases.py
@@ -124,12 +124,14 @@ class LiquidPhase(ThermoPhysicalManager):
         self.vol = vol
         self.moles = moles
 
-        unspec_num = (mass_frac is None) + (mass_conc is None) + (mole_conc is None) + (mole_frac is None)
+        unspec_num = (mass_frac is None) + (mass_conc is None) + \
+            (mole_conc is None) + (mole_frac is None)
 
         if unspec_num == 4:
             raise ValueError("No measure of composition was provided")
         elif unspec_num < 3:
-            raise RuntimeWarning("More than one measure of composition was provided")
+            raise RuntimeWarning("More than one measure of composition was "
+                                 "provided")
 
         if mass_frac is not None:
             self.mass_frac = np.array(mass_frac)