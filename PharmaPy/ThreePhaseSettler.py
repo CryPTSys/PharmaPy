5ba61dfe9113bc7e6fdbb236679a8be6fb25e0fb
varun sundarkumar <vsundark@purdue.edu>
2023-03-21 22:37:17 -0400

    Slurry phases added

--- PharmaPy/ThreePhaseSettler.py
+++ PharmaPy/ThreePhaseSettler.py
@@ -8,10 +8,9 @@ Created on Mon Mar 13 10:51:16 2023
 import numpy as np
 from assimulo.problem import Implicit_Problem
 from PharmaPy.Phases import classify_phases
-from PharmaPy.Streams import VaporStream
 from PharmaPy.Connections import get_inputs_new
-from PharmaPy.Commons import unpack_discretized
 from PharmaPy.Streams import LiquidStream
+from PharmaPy.MixedPhases import Slurry, SlurryStream
 from PharmaPy.Results import DynamicResult
 from PharmaPy.Plotting import plot_distrib
 
@@ -24,24 +23,25 @@ import scipy.sparse
 from matplotlib.ticker import AutoMinorLocator, MaxNLocator
 
 class ThreePhaseSettler:
-    def __init__(self, d_impeller, d_vessel, height_toplayer, 
-                 height_botlayer, agit_rate=1000/60, **other_phys_props):
+    def __init__(self, d_impeller, d_vessel, 
+                 target_compound, agit_rate=1000/60):
 
         self.d_impeller = d_impeller
         self.d_vessel = d_vessel
-        self.height_toplayer = height_toplayer
-        self.height_botlayer = height_botlayer
+        #self.height_toplayer = height_toplayer
+        #self.height_botlayer = height_botlayer
+        self.target_compound = target_compound
         self.agit_rate = agit_rate
         
-        self.holdup_top = np.pi*d_vessel**2/4*height_toplayer
-        self.holdup_bot = np.pi*d_vessel**2/4*height_botlayer
-        self.other_phys_props = other_phys_props
-        self.nomenclature()
+        #self.holdup_top = np.pi*d_vessel**2/4*height_toplayer
+        #self.holdup_bot = np.pi*d_vessel**2/4*height_botlayer
+        #self.other_phys_props = other_phys_props
+        #self.nomenclature()
         return
     
     def nomenclature(self):
-        self.name_states = ['mass_frac']
-        self.states_di = {'mass_frac': {'dim': 1, 'units': 'kg API/kg solvent'}}
+        self.name_states = ['solid_conc']
+        self.states_di = {'solid_conc': {'dim': 1, 'units': 'kg API/kg solvent'}}
         self.states_dict = {'Inlet': self.states_di}
         self.fstates_di = {}
         
@@ -56,6 +56,22 @@ class ThreePhaseSettler:
 
         self._Phases = phases
         classify_phases(self)
+        
+        if self.Liquid_1.getDensity() > self.Liquid_2.getDensity():
+            self.BotLiq = self.Liquid_1
+            self.TopLiq = self.Liquid_2
+        else: 
+            self.BotLiq = self.Liquid_2
+            self.TopLiq = self.Liquid_1
+        path = self.Inlet_bot_phase.path_data
+        self.TopPhase = Slurry(path)
+        self.TopPhase.Phases = [self.TopLiq, self.Solid_1]
+        self.BotPhase = Slurry(path)
+        self.BotPhase.Phases = [self.BotLiq, self.Solid_2]
+        self.holdup_top = self.TopLiq.vol
+        self.holdup_bot = self.BotLiq.vol
+        
+        self.nomenclature()
     
     @property
     def Inlet_feed(self):
@@ -63,8 +79,8 @@ class ThreePhaseSettler:
     @Inlet_feed.setter
     def Inlet_feed(self, inlet_feed):
         self._Inlet_feed = inlet_feed
-        self.name_species = self.Inlet_feed.name_species
-        self.index = self.name_species.index('lomustine_crystals')
+        self.name_species = self.Inlet_feed.Liquid_1.name_species
+        self.index = self.name_species.index(self.target_compound)
         
     @property
     def Inlet_bot_phase(self):
@@ -74,7 +90,7 @@ class ThreePhaseSettler:
         self._Inlet_bot_phase = inlet_bot_phase
     
     def get_input_feed(self, time):
-        inputs = get_inputs_new(time, self.Inlet_feed, self.states_dict)
+        inputs = get_inputs_new(time, self.Inlet_feed.Liquid_1, self.states_dict)
         return inputs
     
     def get_input_bot_phase(self, time):
@@ -87,24 +103,27 @@ class ThreePhaseSettler:
         return material
     
     def design_space_check(self,):
-        d_particle = self.other_phys_props['d_particle']
-        viscosity_top = self.other_phys_props['top_viscosity']
-        density_top = self.other_phys_props['top_density_fluid']
-        density_particle = self.other_phys_props['density_particle']
-        flowrate_feed = self.Inlet_feed.vol_flow
-        gamma_top = self.other_phys_props['top_gamma']
+        d_particle = np.dot(self.Inlet_feed.Solid_1.distrib/sum(self.Inlet_feed.Solid_1.distrib), 
+                            self.Inlet_feed.Solid_1.x_distrib)
+        viscosity_top = self.TopPhase.Liquid_1.getViscosity()
+        density_top = self.TopPhase.Liquid_1.getDensity()
+        density_particle = self.TopPhase.Solid_1.getDensity()
+        flowrate_feed = (self.Inlet_feed.Liquid_1.vol_flow 
+                         + self.Inlet_feed.Solid_1.mass_flow/self.Inlet_feed.Solid_1.getDensity())
+        gamma_top = self.TopPhase.Liquid_1.getSurfTension()
         g = 9.81 #m/s2
         
         # 1st criterion
         vt_top = g*d_particle**2*(density_particle-density_top)/(18*viscosity_top)
         tau_top = self.holdup_top/flowrate_feed
-        vt_crit = 10* self.height_toplayer/tau_top
+        height_toplayer = self.holdup_top/(np.pi*self.d_vessel**2/4)
+        vt_crit = 10* height_toplayer/tau_top
         crit1 = vt_top > vt_crit
         
-        viscosity_bot = self.other_phys_props['bot_viscosity']
-        density_bot = self.other_phys_props['bot_density_fluid']
+        viscosity_bot = self.BotPhase.Liquid_1.getViscosity()
+        density_bot = self.TopPhase.Liquid_1.getDensity()
         flowrate_bot = self.Inlet_bot_phase.vol_flow
-        gamma_bot = self.other_phys_props['bot_gamma']
+        gamma_bot = self.BotPhase.Liquid_1.getSurfTension()
         gamma_int = np.abs(gamma_bot-gamma_top)
         
         d_impeller = self.d_impeller
@@ -113,7 +132,7 @@ class ThreePhaseSettler:
         # 2nd criterion
         Bo = (density_particle-density_bot)*g*d_particle**2/gamma_int
         Ea = np.pi*d_particle**2/4*gamma_int
-        Ek = 0.5*(density_particle*np.pi*d_particle**3/6)*(d_impeller/2*agit_rate/(2*np.pi))
+        Ek = 0.5*(density_particle*np.pi*d_particle**3/6)*(d_impeller/2*agit_rate)
         crit2 = Bo>1 or Ek>Ea
         
         def N_cr_cal(N):
@@ -122,14 +141,14 @@ class ThreePhaseSettler:
                 N=N[-1]
             Re = density_bot*N*d_impeller**2/viscosity_bot
             Fr = N**2*d_impeller/g
-            zeta = 1
-            return d_impeller - d_impeller*Fr*(12.9*(1-Re**-0.11*zeta**0.17)-4.27)
+            zeta = 0.85
+            return zeta - d_impeller*Fr*(12.9*(1-Re**-0.11*zeta**0.17)-4.27)
         
         # 3rd criterion
-        N_cr = scipy.optimize.fsolve(N_cr_cal, x0=agit_rate)
+        N_cr = scipy.optimize.fsolve(N_cr_cal, x0=agit_rate)[0]
         N_js = (234*(d_impeller)**(-2/3)*(d_particle)**(1/3)
                 *((density_particle-density_bot)/density_bot)**(2/3)
-                *(viscosity_bot/density_bot)**(-1/9))
+                *(viscosity_bot*1000/(density_bot/1000))**(-1/9)/60)
         crit3 = agit_rate<N_cr and agit_rate>N_js
         
         # Test
@@ -140,21 +159,22 @@ class ThreePhaseSettler:
         
         return
     
-    def material_balances(self, time, mass_frac):
-        density_top = self.other_phys_props['top_density_fluid']
-        flowrate_feed = self.Inlet_feed.vol_flow
-        density_bot = self.other_phys_props['bot_density_fluid']
+    def material_balances(self, time, solid_conc):
+        density_top = self.TopPhase.Liquid_1.getDensity()
+        flowrate_feed = (self.Inlet_feed.Liquid_1.vol_flow 
+                         + self.Inlet_feed.Solid_1.mass_flow/self.Inlet_feed.Solid_1.getDensity())
+        density_bot = self.BotPhase.Liquid_1.getDensity()
         flowrate_bot = self.Inlet_bot_phase.vol_flow
         
         input_feed = self.get_input_feed(time)['Inlet']
         input_bot_phase = self.get_input_bot_phase(time)['Inlet']
-        mass_frac_feed = input_feed['mass_frac'][self.index]
+        solid_conc_feed = input_feed['solid_conc'][self.index]
 
-        d_mass_frac_dt = ((1/(self.holdup_bot*density_bot))
-                          *(mass_frac_feed*flowrate_feed*density_top
-                            -mass_frac*flowrate_bot*density_bot))
+        d_solid_conc_dt = ((1/(self.holdup_bot*density_bot))
+                          *(solid_conc_feed*flowrate_feed*density_top
+                            -solid_conc*flowrate_bot*density_bot))
         
-        return d_mass_frac_dt
+        return d_solid_conc_dt
 
     def energy_balances(self, time, temp, mole_frac):
         pass
@@ -164,8 +184,8 @@ class ThreePhaseSettler:
         #Check if operating point is in design space
         self.design_space_check()
 
-        init_states = self.Inlet_bot_phase.mass_frac[-1].copy()
-        init_derivative = self.material_balances(time=0, mass_frac=init_states)
+        init_states = self.BotPhase.Solid_1.mass/(self.BotPhase.Liquid_1.mass+self.BotPhase.Solid_1.mass)
+        init_derivative = self.material_balances(time=0, solid_conc=init_states)
 
         problem = Implicit_Problem(self.unit_model, init_states, init_derivative, t0)
         solver = IDA(problem)
@@ -181,16 +201,16 @@ class ThreePhaseSettler:
         indexes = {key: self.states_di[key].get('index', None)
                    for key in self.name_states}
 
-        dp = {'time': time, 'mass_frac': states}
+        dp = {'time': time, 'solid_conc': states}
         self.result = DynamicResult(di_states=self.states_di, di_fstates=None,**dp)
         self.outputs = dp
         
         # Outlet stream
-        path = self.Inlet_bot_phase.path_data
-        mass_frac = self.Inlet_bot_phase.mass_frac
-        mass_frac[-1] = dp['mass_frac'][-1][-1]
-        mass_frac[-2] = mass_frac[-2] - dp['mass_frac'][-1][-1]
-        temp=self.Inlet_bot_phase.temp
-        self.OutletBottom = LiquidStream(
-            path, mass_frac=mass_frac,  vol_flow=self.Inlet_bot_phase.vol_flow)
-        self.Outlet = self.OutletBottom
+        # path = self.Inlet_bot_phase.path_data
+        # solid_conc = self.Inlet_bot_phase.mass_frac
+        # mass_frac[-1] = dp['mass_frac'][-1][-1]
+        # mass_frac[-2] = mass_frac[-2] - dp['mass_frac'][-1][-1]
+        # temp=self.Inlet_bot_phase.temp
+        # self.OutletBottom = LiquidStream(
+        #     path, mass_frac=mass_frac,  vol_flow=self.Inlet_bot_phase.vol_flow)
+        # self.Outlet = self.OutletBottom
