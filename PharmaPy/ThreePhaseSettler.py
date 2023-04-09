TPS d_particle calculation fixed)

PharmaPy/ThreePhaseSettler.py
@@ -96,8 +96,18 @@ class ThreePhaseSettler:
         return material
 
     def design_space_check(self,):
-        d_particle = np.dot(self.Inlet_feed.Solid_1.distrib/sum(self.Inlet_feed.Solid_1.distrib),
-                            self.Inlet_feed.Solid_1.x_distrib/1e6)
+        
+        #calculate mean diameter
+        distrib = self.Inlet_feed.Solid_1.distrib
+        x_distrib = self.Inlet_feed.Solid_1.x_distrib
+        perc_vol_basis = np.percentile(distrib*x_distrib**3*1e-9, 50, method='closest_observation') #distrib**3 to get volume basis, 1e-9 to convert to m3 and reduce number magnitude
+        D50_index = np.where(distrib*x_distrib**3*1e-9==perc_vol_basis)[0][0]
+        D50 = x_distrib[D50_index]
+        
+        d_particle = D50*1e-6
+        
+        # d_particle = np.dot(self.Inlet_feed.Solid_1.distrib/sum(self.Inlet_feed.Solid_1.distrib),
+        #                     self.Inlet_feed.Solid_1.x_distrib/1e6)
         viscosity_top = self.TopPhase.Liquid_1.getViscosity()
         density_top = self.TopPhase.Liquid_1.getDensity()
         density_particle = self.TopPhase.Solid_1.getDensity()
@@ -124,7 +134,7 @@ class ThreePhaseSettler:
         # 2nd criterion
         Bo = (density_particle-density_bot)*g*d_particle**2/gamma_int
         Ea = np.pi*d_particle**2/4*gamma_int
-        Ek = 0.5*(density_particle*np.pi*d_particle**3/6)*(d_impeller/2*agit_rate)
+        Ek = 0.5*(density_particle*np.pi*d_particle**3/6)*(d_impeller/2*(agit_rate/2*np.pi))
         crit2 = Bo>1 or Ek>Ea
 
         def N_cr_cal(N):
@@ -145,9 +155,9 @@ class ThreePhaseSettler:
 
         # Test
         if crit1 and crit2 and crit3:
-            print('Operating point is in design space')
+            print('TPS: operating point is in design space')
         else:
-            print('Operating point outside design space')
+            print('TPS: operating point is not in design space')
 
         return
 