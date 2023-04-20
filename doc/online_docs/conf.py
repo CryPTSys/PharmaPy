a134ccc283fb1f50aa8049e79f217a894f810179
Daniel Mauricio Casas-Orozco <dcasasor@purdue.edu>
2023-04-20 15:05:16 -0400

    Adding example notebooks to the documentation

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -32,7 +32,7 @@ release = '2023'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
