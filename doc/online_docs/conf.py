e1d38683dccbfe96623c78b357d533e667d96310
Daniel Casas-Orozco <dcasasor@purdue.edu>
2023-04-28 18:59:34 -0400

    Installation description was simplified. Also, references to PharmaPy papers were added.

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -32,8 +32,10 @@ release = '2023'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 
+extensions = ['sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 'sphinxcontrib.bibtex' 
 ]
+
+bibtex_bibfiles = ['references.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
