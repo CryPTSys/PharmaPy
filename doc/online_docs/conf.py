3c0e4d425e75d29c61600cce0a30c5c83845c5c3
rhimj <86616174+rhimj@users.noreply.github.com>
2023-06-05 18:29:14 -0400

    Update conf.py
    Implementation of mock libraries

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -4,6 +4,14 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# Setup of mock libraries to override the build fails for C based libraries
+
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
