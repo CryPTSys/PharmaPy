6bf884261a4889acf35f25ee6b82b37c01c6dc3e
rhimj <86616174+rhimj@users.noreply.github.com>
2023-06-05 18:41:17 -0400

    Update conf.py
    Formatting

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -10,7 +10,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'assimulo']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+ sys.modules[mod_name] = mock.Mock()
 
 # -- Path setup --------------------------------------------------------------
 
