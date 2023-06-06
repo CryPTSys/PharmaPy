9e47ef160acd80d87fed738dca8e6d0191d0410d
rhimj <86616174+rhimj@users.noreply.github.com>
2023-06-05 21:15:18 -0400

    Update conf.py
    Included 'nbsphinx' to the list of mock libraries

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'assimulo']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'assimulo', 'nbsphinx']
 for mod_name in MOCK_MODULES:
  sys.modules[mod_name] = mock.Mock()
 
