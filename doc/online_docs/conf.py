c8607925088c39c71d7cc44133cd9f0e58f8fa98
rhimj <86616174+rhimj@users.noreply.github.com>
2023-06-05 23:34:48 -0400

    Update conf.py
    Changed the static path so that the build properly imports images.

--- doc/online_docs/conf.py
+++ doc/online_docs/conf.py
@@ -66,7 +66,7 @@ html_logo = 'images/PharmaPy_logo.jpeg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static','images']
 html_context = {
     "footer_logos": {
         "row1": [
